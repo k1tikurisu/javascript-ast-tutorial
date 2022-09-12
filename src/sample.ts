25fa0f777cc9eb7d6aece588bc491c1e2ef51fee
Daiki Maekawa <daaikii@outlook.jp>
2022-09-12 18:28:13 +0900

    add typescript

---
+++ src/sample.ts
@@ -0,0 +1,22 @@
+import { transform, types as t } from "@babel/core";
+import type { NodePath } from "@babel/core";
+
+const src = "1 + 2";
+
+const plugin = () => ({
+  visitor: {
+    BinaryExpression: (nodePath: NodePath<t.BinaryExpression>) => {
+      if (nodePath.node.operator !== "*") {
+        const newAst = t.binaryExpression(
+          "*",
+          nodePath.node.left,
+          nodePath.node.right
+        );
+        nodePath.replaceWith(newAst);
+      }
+    },
+  },
+});
+
+const result = transform(src, { plugins: [plugin] });
+console.log(result?.code); // --> 1 * 2;
