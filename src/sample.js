47d68af4b5b5d499d40b980ae401c65a45a51de9
Daiki Maekawa <daaikii@outlook.jp>
2022-09-12 17:16:48 +0900

    done pre

---
+++ src/sample.js
@@ -0,0 +1,17 @@
+import { transform } from '@babel/core'
+
+const src = '1 + 2'
+
+const plugin = ({ types: t }) => ({
+  visitor: {
+    BinaryExpression: (nodePath) => {
+      if (nodePath.node.operator !== '*') {
+        const newAst = t.binaryExpression('*', nodePath.node.left, nodePath.node.right)
+        nodePath.replaceWith(newAst)
+      }
+    }
+  }
+})
+
+const { code } = transform(src, { plugins: [plugin] })
+console.log(code) // --> 1 * 2;
