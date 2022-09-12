f137a31e0e477271f02ca21ec7670f367bef32f7
Daiki Maekawa <daaikii@outlook.jp>
2022-09-12 18:29:48 +0900

    add prettier config file

--- src/sample.ts
+++ src/sample.ts
@@ -1,22 +1,22 @@
-import { transform, types as t } from "@babel/core";
-import type { NodePath } from "@babel/core";
+import { transform, types as t } from '@babel/core'
+import type { NodePath } from '@babel/core'
 
-const src = "1 + 2";
+const src = '1 + 2'
 
 const plugin = () => ({
   visitor: {
     BinaryExpression: (nodePath: NodePath<t.BinaryExpression>) => {
-      if (nodePath.node.operator !== "*") {
+      if (nodePath.node.operator !== '*') {
         const newAst = t.binaryExpression(
-          "*",
+          '*',
           nodePath.node.left,
           nodePath.node.right
-        );
-        nodePath.replaceWith(newAst);
+        )
+        nodePath.replaceWith(newAst)
       }
-    },
-  },
-});
+    }
+  }
+})
 
-const result = transform(src, { plugins: [plugin] });
-console.log(result?.code); // --> 1 * 2;
+const result = transform(src, { plugins: [plugin] })
+console.log(result?.code) // --> 1 * 2;
